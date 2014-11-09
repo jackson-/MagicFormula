7ff6f4de62ff3cb668bd3160a4014a409aea3e1e
jackson- <jackson.t.devin@gmail.com>
2014-11-09 08:40:33 -0500

    got pe ratio in array. having problems pulling it out. weird

--- app/formula/views.py
+++ app/formula/views.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup as bs4
 import requests as r
 from formula.forms import FormulaForm
 from django.http import HttpResponse, JsonResponse
+import json
+import re
 
 # Create your views here.
 class IndexView(View):
@@ -11,21 +13,33 @@ class IndexView(View):
 	form_class = FormulaForm
 
 	def get(self, request):
-		
 		return render(request, self.template_name, {'form':self.form_class})
 
 class MagicView(View):
+	lookup_url = "http://dev.markitondemand.com/Api/v2/Lookup/json?input="
+	quote_url = "http://dev.markitondemand.com/Api/v2/Quote/json?symbol="
 
 	def post(self, request):
-		print(request.POST)
 		symbols = request.POST['symbols'].split(',')
 		for value in range(len(symbols)):
 			symbols[value] = symbols[value].strip()
 		roc = []
 		for value in symbols:
-			url = r.get("http://www.gurufocus.com/term/ROC_JOEL/" + value + "/Return%252Bon%252BCapital%252B%252B-%252BJoel%252BGreenblatt/")
+			value = value.upper()
+			# result = r.get(self.lookup_url + value).json()
+			url = r.get("http://174.129.18.141/companies/GOOG/pe_ratio")
 			soup = bs4(url.text)
-			for div in soup.select('.data_value'):
-				roc.append(div.get_text()[:-18])
-		print(roc)
+			thing = soup.select('span#pgNameVal')
+			thing = thing[0].text
+			thing = re.split('\s+', thing)
+
+			print(thing[:1])
+			# for div in soup.select('.Es'):
+			# 	print(div.get_text())
+			# url = r.get("http://www.gurufocus.com/term/ROC_JOEL/" + value + "/Return%252Bon%252BCapital%252B%252B-%252BJoel%252BGreenblatt/")
+			# soup = bs4(url.text)
+			# for div in soup.select('.data_value'):
+			# 	roc.append(div.get_text()[:-18])
+
+		url = "http://www.wikinvest.com/stock/Symantec_(SYMC)/Data/P:E"
 		return HttpResponse('test')
