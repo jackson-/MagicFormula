fe892cec2defc7df88d3846fd333318954361613
jackson- <jackson.t.devin@gmail.com>
2014-11-09 21:35:51 -0500

    issues

--- app/formula/static/formula/app.js
+++ app/formula/static/formula/app.js
@@ -2,9 +2,6 @@ $(document).ready(function(){
 	$('.form').submit(function(e){
 		e.preventDefault()
 		$.post('/magic/', $('.form').serialize(), function(data){
-			
-			// $('.output').append('<li>' + data['ibm'] + '</li>', OnSubmitCreateForm('.content', data))
 		}, 'json')
 	})
-
 })
